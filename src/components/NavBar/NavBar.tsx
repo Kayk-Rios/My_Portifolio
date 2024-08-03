5d5477c884bc37d61471fc7223a6643102f0fa37
Kayk-Rios <manjac688@gmail.com>
2024-08-02 22:15:15 -0300

    Atulizando menu-currriculo

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,44 +1,79 @@
 import { MenuItem } from "@mui/material";
+import { useState } from "react";
 import "../../project.css";
 const NavBar = () => {
-    const handleMenuClick = (id: string) => {
-        const element = document.getElementById(id);
-        if (element) {
-            element.scrollIntoView({ behavior: "smooth" });
-        }
-    };
+  const [active, setMode] = useState(false);
 
-    return (
-        <>
-            <div>
-                <nav className="navbar">
-                    <div className="max-width">
-                        <ul className="menu">
-                            <li>
-                                {" "}
-                                <MenuItem onClick={() => handleMenuClick("about")}>
-                                    About
-                                </MenuItem>{" "}
-                            </li>
-                            <li>
-                                {" "}
-                                <MenuItem onClick={() => handleMenuClick("skills")}>
-                                    Skills
-                                </MenuItem>
-                            </li>
-                            <li>
-                                {" "}
-                                <MenuItem onClick={() => handleMenuClick("projects")}>
-                                    Project
-                                </MenuItem>{" "}
-                            </li>
+  const ToggleMode = () => {
+    setMode(!active);
+  };
 
-                        </ul>
-                    </div>
-                </nav>
+  const handleMenuClick = (id: string) => {
+    const element = document.getElementById(id);
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
+  return (
+    <>
+      <div>
+        <nav className="navbar">
+          <div className="max-width">
+            <ul className="menu">
+              <li>
+                <MenuItem onClick={() => handleMenuClick("about")}>
+                  About
+                </MenuItem>
+              </li>
+              <li>
+                <MenuItem onClick={() => handleMenuClick("skills")}>
+                  Skills
+                </MenuItem>
+              </li>
+              <li>
+                <MenuItem onClick={() => handleMenuClick("projects")}>
+                  Project
+                </MenuItem>
+              </li>
+            </ul>
+          </div>
+        </nav>
+
+        <div className="app2">
+          <div className="app1 none">
+            <div
+              className={active ? "icon iconActive " : "icon"}
+              onClick={ToggleMode}
+            >
+              <div className="hamburguer hamburgueIcon"></div>
             </div>
-        </>
-    );
+            <div className={active ? "menu menuOpen" : "menu menuClose"}>
+              <div className="list">
+                <ul className="listItems">
+                  <li>
+                    <MenuItem onClick={() => handleMenuClick("about")}>
+                      About
+                    </MenuItem>
+                  </li>
+                  <li>
+                    <MenuItem onClick={() => handleMenuClick("skills")}>
+                      Skills
+                    </MenuItem>
+                  </li>
+                  <li>
+                    <MenuItem onClick={() => handleMenuClick("projects")}>
+                      Project
+                    </MenuItem>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default NavBar;
--- src/pages/Home/Section/Hero/Hero.tsx
+++ src/pages/Home/Section/Hero/Hero.tsx
@@ -10,7 +10,7 @@ import Avatar from "../../../../assets/images/avatar.jpg";
 import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import { AnimatedBackground } from "../../../../components/AnimatedBackground/AnimatedBackground";
-import downloadLink from "../../../../Download/currículo.pdf";
+import downloadLink from "../../../../Download/currículo2.pdf";
 import "../../../../App.css"
 import GitHubIcon from '@mui/icons-material/GitHub';
 import Typical from 'react-typical'
@@ -114,7 +114,7 @@ const Hero = () => {
                 >
                   <StyledButton>
                     <ArrowDownwardIcon />
-                    <Typography><a href={downloadLink} download='currículo.pdf' style={{ textDecoration: 'none', color: 'inherit' }}>Download CV</a></Typography>
+                    <Typography><a href={downloadLink} download='currículo2.pdf' style={{ textDecoration: 'none', color: 'inherit' }}>Download CV</a></Typography>
                   </StyledButton>
                 </Grid>
                 <Grid

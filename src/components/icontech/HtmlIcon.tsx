dc38ab09a034ff294245afecf0a1d475aa49a843
Kayk-Rios <manjac688@gmail.com>
2024-10-15 20:52:24 -0300

    chore:componentesIconsTecnologias

---
+++ src/components/icontech/HtmlIcon.tsx
@@ -0,0 +1,30 @@
+ 
+export default function HtmlIcon(){
+    return(
+        <>
+         <div className="svg-container">
+           <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="ionicon"
+                    viewBox="0 0 512 512"
+                    width="50"
+                    height="50"
+                    fill="#1572B6"
+                  >
+                    <path d="M64 32l35 403.22L255.77 480 413 435.15 448 32zm290.68 334.9L256.07 395l-98.46-28.24-6.75-77.76h48.26l3.43 39.56 53.59 15.16.13.28 53.47-14.85 5.64-64.15H203l-4-50h120.65l4.35-51H140l-4-49h240.58z" />
+                  </svg>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="ionicon"
+                    viewBox="0 0 512 512"
+                    width="50"
+                    height="50"
+                    fill="#E34F26"
+                  >
+                    <path d="M64 32l34.94 403.21L255.77 480 413 435.15 448 32zm308 132H188l4 51h176l-13.51 151.39L256 394.48l-98.68-28-6.78-77.48h48.26l3.42 39.29L256 343.07l53.42-14.92L315 264H148l-12.59-149.59H376.2z" />
+                  </svg>
+         </div>
+        
+        </>
+    )
+}

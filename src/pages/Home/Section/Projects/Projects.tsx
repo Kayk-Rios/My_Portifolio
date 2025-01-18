811cc0cae953546f9db8d0c93b073a9b10258a1b
Kayk-Rios <manjac688@gmail.com>
2025-01-18 18:15:12 -0300

    animateCardGalstore

--- src/pages/Home/Section/Projects/Projects.tsx
+++ src/pages/Home/Section/Projects/Projects.tsx
@@ -193,7 +193,7 @@ export default function Projects() {
 
 
         <motion.div
-          initial={{ opacity: 0, x: 100 }}
+          initial={{ opacity: 0, x: -100 }}
           animate={{ opacity: 1, y: 0 }}
           whileInView={{ opacity: 1, x: 0 }}
           transition={{ duration: 0.5 }}

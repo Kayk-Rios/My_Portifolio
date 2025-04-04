283aae416b17d4a5d69d00ad749e157f1cefe8e2
Kayk_Darios <manjac688@gmail.com>
2025-04-04 02:04:14 -0300

    feat:criando footer

---
+++ src/pages/Home/Section/Footer/Footer.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="portfolio-footer">
+      <div className="footer-content">
+        <h2 className="footer-name">Kayk Dario</h2>
+
+        
+        <div className="footer-socials">
+          <a href="https://github.com/Kayk-Rios" target="_blank" rel="noopener noreferrer">GitHub</a>
+          <a href="https://www.linkedin.com/in/kayk-dario/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+          <a href="mailto:menosum48@gmail.com">E-mail</a>
+        </div>
+      </div>
+
+      <div className="footer-bottom">
+        © {new Date().getFullYear()} Kayk Dario • Todos os direitos reservados.
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;

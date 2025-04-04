871a2f5dac189833f434582a7d34b8480048bf15
Kayk_Darios <manjac688@gmail.com>
2025-04-04 02:04:35 -0300

    chore:Adcionando componente footer

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,25 +1,21 @@
-import NavBar from "../../components/NavBar/NavBar"
-import About from "./Section/About/About"
-import Hero from "./Section/Hero/Hero"
-import Projects from "./Section/Projects/Projects"
-import { Skills } from "./Section/Skills/Skills"
+import NavBar from "../../components/NavBar/NavBar";
+import About from "./Section/About/About";
+import Footer from "./Section/Footer/Footer";
+import Hero from "./Section/Hero/Hero";
+import Projects from "./Section/Projects/Projects";
+import { Skills } from "./Section/Skills/Skills";
 
+const Home = () => {
+  return (
+    <>
+      <NavBar />
+      <Hero />
+      <About />
+      <Skills />
+      <Projects />
+      <Footer />
+    </>
+  );
+};
 
-const Home = ( ) => {
-
-
-    return (
-
-        <>
-            <NavBar />
-            <Hero />
-            <About />
-            <Skills/>
-           <Projects/>
-           
-        </>
-
-    )
-}
-
-export default Home
+export default Home;

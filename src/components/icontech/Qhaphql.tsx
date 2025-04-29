a8e04c2df89069444a733bbc9a627a8fb9c35cfd
Kayk_Darios <manjac688@gmail.com>
2025-04-29 15:30:14 -0300

    feat: criando stack adquirida QraphQl

---
+++ src/components/icontech/Qhaphql.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Qhaphql() {
+  return (
+    <>
+    <img src="https://www.vectorlogo.zone/logos/graphql/graphql-icon.svg" alt="Logo" width={50} height={50}/>;
+    </>
+  );
+}
